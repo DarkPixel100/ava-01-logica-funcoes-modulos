cc2e9aee5a03dd4ac5c38fb06ec4d6237663fe01
DarkPixel100 <diegofavila20@gmail.com>
2021-09-24 22:38:33 -0300

    Re-formatting

--- contaCaractere.js
+++ contaCaractere.js
@@ -1,9 +1,7 @@
 export function contaCaractere(string, caractere) {
   let resp = 0;
   for (let i of string) {
-    if (i === caractere) {
-      resp++;
-    }
+    if (i === caractere) resp++;
   }
   return resp;
 }
